1ae6dce2907a54d786288bcfd90beb7fa3c3e2f4
f-genius <89853627+f-genius@users.noreply.github.com>
2023-11-29 17:04:58 +0300

    Initial commit

---
+++ src/test/java/ru/shev/time_tracker/TimeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shev.time_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
